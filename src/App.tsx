dd8a672270c09bbcb389fb58eab4dad4f96e722b
Infowil <info@thewitslab.com>
2022-11-23 23:50:01 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from 'react-query'
+import logo from './logo.svg'
+import './App.css'
+
+const queryClient = new QueryClient()
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  </QueryClientProvider>
+)
+
+export default App
