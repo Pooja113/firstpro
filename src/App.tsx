7b2b780f3425eb89a863c9d2c97e7cfd429a4fea
Sahil Sharma <sahil.sharma@thewitslab.com>
2022-12-08 01:40:11 +0530

    feat: secure routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,28 @@ import Registration from 'pages/Registration'
 import Dashboard from 'pages/Dashboard'
 import FailPage from 'pages/FailPage'
 import Login from 'pages/Login'
+import ProtectedRoutes from 'components/ProtectedRoutes'
 
 const queryClient = new QueryClient()
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Registration />} />
-        <Route path="instructions" element={<Instructions />} />
-        <Route path="test" element={<Home />} />
-        <Route path="thankyou" element={<Thankyou />} />
-        <Route path="sorry" element={<FailPage />} />
-        <Route path="admin" element={<Login />} />
-        <Route path="admin/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  </QueryClientProvider>
-)
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Registration />} />
+          <Route path="/" element={<ProtectedRoutes />}>
+            <Route path="instructions" element={<Instructions />} />
+            <Route path="test" element={<Home />} />
+            <Route path="thankyou" element={<Thankyou />} />
+            <Route path="sorry" element={<FailPage />} />
+            <Route path="admin" element={<Login />} />
+            <Route path="admin/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
 
 export default App
