6c2a9042cba1240e43e888f33bb5f42dae63bd25
Abhijeet Singh Rathor <abhijeet@Wil-Admins-MacBook-Air-2.local>
2022-12-08 11:02:15 +0530

    fix: patch fixed

--- src/components/InstructionList.tsx
+++ src/components/InstructionList.tsx
@@ -19,18 +19,19 @@ import {
 } from 'styles/components/InstructionList'
 import { useNavigate } from 'react-router-dom'
 import ROUTES from 'routes'
-// import usePatch from 'hooks/usePatch'
+import usePatch from 'hooks/usePatch'
 import Modal from 'components/Modal'
 
 import instructions from '../assets/data/instruction.json'
 import usePost from 'hooks/usePost'
 import ErrorModal from './ErrorModal'
-import 'react-html5-camera-photo/build/css/index.css'
 import { LoaderContext } from 'context/loader'
+import 'react-html5-camera-photo/build/css/index.css'
 
 const InstructionList = () => {
   const navigate = useNavigate()
   const [isChecked, setIsChecked] = useState(false)
+  const { mutateAsync: patchAsync } = usePatch()
   const { mutateAsync } = usePost()
   const [modal, setModal] = useState(false)
   const [showCamera, setShowCamera] = useState(false)
@@ -66,7 +67,7 @@ const InstructionList = () => {
 
   const handleTakePhoto = async (base64: string) => {
     try {
-      await mutateAsync({ url: 'user/addPhoto', payload: { photo: base64 }, token: true })
+      await patchAsync({ url: 'user/addPhoto', payload: { photo: base64 }, token: true })
       setShowCamera(false)
       navigate(`${ROUTES?.TEST?.LINK}`, { replace: true })
     } catch (error: any) {
