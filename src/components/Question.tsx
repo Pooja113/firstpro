46b91c14bff78fa7d2ef809e8f4bdbd8976884f9
Pooja Paul <poojapaul@WILASSETA00145.local>
2022-12-09 18:55:19 +0530

    fix: question code fixed and index added

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -10,6 +10,12 @@ import {
 } from 'styles/components/Question'
 
 const Question = (props: any) => {
+  const question = props.data.question
+  const sliceStartIndex = question.indexOf('<pre>')
+  const sliceLastIndex = question.indexOf('</pre>')
+
+  const res = question.slice(sliceStartIndex + 5, sliceLastIndex)
+
   const handleClick: ReactEventHandler<HTMLInputElement> = (evnt) => {
     if (!evnt.currentTarget.checked) {
       props.answerFunc(evnt.currentTarget.id, [-1])
@@ -20,11 +26,17 @@ const Question = (props: any) => {
 
   return (
     <>
-      <EachQuestion>{props.data.question}</EachQuestion>
-      {props.data.code && (
-        <CodeContainer>
-          <pre>{props.data.code}</pre>
-        </CodeContainer>
+      {props.data.question.includes('<code><pre>') ? (
+        <>
+          <EachQuestion>{props.index + 1}. What is the output of the following code?</EachQuestion>
+          <CodeContainer>
+            <pre>{res}</pre>
+          </CodeContainer>
+        </>
+      ) : (
+        <EachQuestion>
+          {props.index + 1}. {props.data.question}
+        </EachQuestion>
       )}
       <AnswerList>
         {props.data.answers.map((each: any, idx: number) => (
