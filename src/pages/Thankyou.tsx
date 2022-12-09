88777e507a3464c6ca4725d4d399e1b8b15ba49e
Tarun2508 <tarun.kumar@thewitslab.com>
2022-12-09 18:23:16 +0530

    fix: thankyou navigation fixed

--- src/pages/Thankyou.tsx
+++ src/pages/Thankyou.tsx
@@ -1,9 +1,18 @@
 import Footer from 'components/Footer'
 import Header from 'components/Header'
 import Score from 'components/Score'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import ROUTES from 'routes'
 
 const Thankyou = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    window.onblur = () => {
+      navigate(`${ROUTES?.THANKYOU?.LINK}`, { replace: true })
+    }
+  }, [])
   return (
     <>
       <Header />
