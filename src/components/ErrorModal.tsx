b0bb2dc831f8cdb938316b6f47d069b36f1cfd63
yatin97 <yatin.sharma@thewitslab.com>
2022-12-12 20:17:20 +0530

    fix: bug fixes with validation

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -3,8 +3,12 @@ import Modal from 'react-modal'
 
 import { ModalTitle } from 'styles/components/ErrorModal'
 
+const customStyles = {
+  overlay: { zIndex: '9' },
+}
+
 const ErrorModal = ({ isOpen, close, error }: any) => (
-  <Modal isOpen={isOpen} onRequestClose={close} contentLabel="Example Modal">
+  <Modal isOpen={isOpen} onRequestClose={close} contentLabel="Example Modal" style={customStyles}>
     <button onClick={close}>close</button>
     <ModalTitle>{error}</ModalTitle>
   </Modal>
